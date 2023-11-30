7c254da4a8c8def604720e8ded6b4f0d5e141973
tungit04315 <93088015+tungit04315@users.noreply.github.com>
2023-11-30 12:39:03 +0700

    commit test

--- src/main/java/com/poly/controller/FacebookShareController.java
+++ src/main/java/com/poly/controller/FacebookShareController.java
@@ -1,7 +1,10 @@
 package com.poly.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +15,9 @@ public class FacebookShareController {
 
     @Autowired
     HttpServletRequest req;
+    
+    @Autowired
+    HttpSession ss;
 
     private static final String FACEBOOK_SHARE_URL = "https://www.facebook.com/dialog/share";
     private static final String APP_ID = "1035305550719180";
@@ -30,7 +36,7 @@ public class FacebookShareController {
     @GetMapping("/share-on-facebook")
     @ResponseBody
     public String shareOnFacebook() {
-        String url = req.getRequestURL().toString();
+        String url = (String) ss.getAttribute("url");
         System.out.println(url);
         String shareUrl = FACEBOOK_SHARE_URL +
                 "?app_id=" + APP_ID +
@@ -42,6 +48,8 @@ public class FacebookShareController {
 
     @GetMapping("/")
     public String getHome() {
+    	String url = req.getRequestURL().toString();
+    	ss.setAttribute("url", url);
         return "index";
     }
 }
